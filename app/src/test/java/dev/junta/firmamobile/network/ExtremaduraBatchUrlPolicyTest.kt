43a3dc288c7e84e7b86e3dc7aad5536745be2441
LooneyMad <zaguzovmaksim0@gmail.com>
2026-08-12 00:28:05 +0200

    test(portal): require fixed Extremadura STA URL policy

---
+++ app/src/test/java/dev/junta/firmamobile/network/ExtremaduraBatchUrlPolicyTest.kt
@@ -0,0 +1,56 @@
+package dev.junta.firmamobile.network
+
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class ExtremaduraBatchUrlPolicyTest {
+    private val melilla = MelillaBatchUrlPolicy()
+    private val extremadura = ExtremaduraBatchUrlPolicy()
+
+    @Test
+    fun fixedProfilesOwnOnlyTheirExactStaRuntimeBindings() {
+        val operationId = "runtime-operation-1"
+        val documentId = "runtime-document-1"
+
+        val melillaPre =
+            "https://sede.melilla.es/sta/AutofirmaLote/presign/$operationId"
+        val melillaPost =
+            "https://sede.melilla.es/sta/AutofirmaLote/postsign/$operationId"
+        val melillaData =
+            "https://sede.melilla.es/sta/AutofirmaLote/getdata/$operationId/$documentId"
+        val extremaduraPre =
+            "https://tramites.juntaex.es/sta/AutofirmaLote/presign/$operationId"
+        val extremaduraPost =
+            "https://tramites.juntaex.es:443/sta/AutofirmaLote/postsign/$operationId"
+        val extremaduraData =
+            "https://tramites.juntaex.es/sta/AutofirmaLote/getdata/$operationId/$documentId"
+
+        assertNotNull(melilla.validatePreSignerUrl(melillaPre))
+        assertNotNull(melilla.validatePostSignerUrl(melillaPost))
+        assertNotNull(melilla.validateDataReference(melillaData, operationId, documentId))
+        assertNotNull(extremadura.validatePreSignerUrl(extremaduraPre))
+        assertNotNull(extremadura.validatePostSignerUrl(extremaduraPost))
+        assertNotNull(extremadura.validateDataReference(extremaduraData, operationId, documentId))
+
+        assertNull(melilla.validatePreSignerUrl(extremaduraPre))
+        assertNull(extremadura.validatePreSignerUrl(melillaPre))
+        assertNull(extremadura.validatePreSignerUrl(extremaduraPost))
+        assertNull(extremadura.validatePostSignerUrl(extremaduraPre))
+        assertNull(extremadura.validateDataReference(extremaduraPre, operationId, documentId))
+        assertNull(
+            extremadura.validateDataReference(
+                extremaduraData,
+                expectedOperacionId = "other-operation",
+                expectedDocId = documentId,
+            ),
+        )
+        assertNull(
+            extremadura.validateDataReference(
+                extremaduraData,
+                expectedOperacionId = operationId,
+                expectedDocId = "other-document",
+            ),
+        )
+    }
+}
